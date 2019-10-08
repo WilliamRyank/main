6286cac13dfacdf327b41ab57f89364bd843f56a
Chow Jia Ying <chowjiaying211@gmail.com>
2019-10-08 15:24:56 +0800

    Change unit test for Age.java in AgeTest.java

--- src/test/java/seedu/address/model/person/AgeTest.java
+++ src/test/java/seedu/address/model/person/AgeTest.java
@@ -33,15 +33,17 @@ public class AgeTest {
         assertFalse(Age.isValidAge(null)); //null age
         assertFalse(Age.isValidAge("200")); //over the range of 0-150
         assertFalse(Age.isValidAge("0.8")); //decimals
+        assertFalse(Age.isValidAge("0")); //0 case
+        assertFalse(Age.isValidAge("150")); //max valid age
 
         // valid age
-        assertTrue(Age.isValidAge("0")); //0 case
+        assertTrue(Age.isValidAge("1")); //min valid age
         assertTrue(Age.isValidAge("6")); //single digit number
         assertTrue(Age.isValidAge("27")); //double digit number
         assertTrue(Age.isValidAge("10")); //starts with 1
         assertTrue(Age.isValidAge("30")); //starts with 3
         assertTrue(Age.isValidAge("67")); //starts with 6
         assertTrue(Age.isValidAge("99")); //starts with 9
-        assertTrue(Age.isValidAge("150")); //max valid age
+        assertTrue(Age.isValidAge("149")); //max valid age
     }
 }
